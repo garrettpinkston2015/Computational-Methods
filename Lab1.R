b5c18f2103fc249f3ed10c41e701a1b883cedb2d
Garrett Pinkston <garrettpinkston@Garretts-MacBook-Pro-3.local>
2024-09-05 17:48:45 -0400

    IQR code beginning to remove income outliers

--- Lab1.R
+++ Lab1.R
@@ -4,20 +4,25 @@ setwd('/Users/garrettpinkston/Desktop/Michigan/STAT506/Data/')
 df <- read.csv('AskAManager.csv')
 
 names(df)
-df$X = NULL
+df$X = NULL #remove first column (junk) dataframe
+
 descriptors <- c("Timestamp","Age","Industry","Title","Additional Title Info",
                  "Salary","Bonus Pay","Currency","Other Currency","Additional Income Info",
                  "Country", "State", "City", "Total Experience", "Industry Experience", 
                  "Education", "Gender", "Race")
 
-colnames(df) <- descriptors
+colnames(df) <- descriptors #replace column names with accurate descriptions
 df <- df[df$Currency == 'USD',] # Keep all entries of just USD
-
 
 df$MinimumExperience <- as.numeric(gsub(".*?([0-9]+).*", "\\1", df$`Total Experience`)) # found code on stackoverflow with RegEx to help
 df$MinimumAge <- as.numeric(gsub(".*?([0-9]+).*", "\\1", df$Age)) # found code on stackoverflow with RegEx to help
-
 df <- df[(df$MinimumAge - df$MinimumExperience) > 18,] #filter all rows where people started work before 18
+
+# Will use IQR to remove outliers
+quartiles <- quantile(df$Salary, probs = c(0.25,0.75)) #only returns Q1 and Q3 as a list
+salaryRange <- IQR(df$Salary) #returns scalar
+lowerLimit <- quartiles[1] - (1.5*salaryRange) # lower than first quartile
+upperLimit <- quartiles[2] + (1.5*salaryRange) # above third quartile
 
 
 # Palindromes
